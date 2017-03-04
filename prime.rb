fc49682e880cee24314293f4b5297ad5f918693a
Curtis Greene <curtis.j.w.greene@gmail.com>
2017-03-04 19:54:54 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def eratosthenes(int)
+  prime_numbers = (0..int).to_a
+  prime_numbers[0] = nil
+  prime_numbers[1] = nil
+  counter = 0
+  prime_numbers.each do |p|
+    next unless p # skips nil/known prime numbers
+    break if p*p > int #if the square is outside the range
+    counter += 1
+    (p * p).step(int, p) { |m| prime_numbers[m] = nil}
+  end
+  puts "Solved for #{int} in #{counter} steps"
+  prime_numbers.compact
+end
+
+def prime?(n)
+  eratosthenes(n).include?(n)
+end
